15b22c5ef181a198a43f662b3a707fa2c41317d3
Kevin Karma <kevinkarma55@gmail.com>
2019-09-06 20:03:25 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,6 +1,6 @@
 class CreateStudents < ActiveRecord::Migration[5.2]
   def change
-    create_table :creat_students do |t|
+    create_table :Create_students do |t|
       t.string :name 
     end
   end
